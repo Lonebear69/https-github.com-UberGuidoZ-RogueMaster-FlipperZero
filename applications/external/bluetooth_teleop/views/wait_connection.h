05d64ca1262050fe0b938e66eacd831d3659aff5
RogueMaster <anifan115@gmail.com>
2023-03-22 02:05:11 -0400

    Remove App w Issue

--- applications/external/bluetooth_teleop/views/wait_connection.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <gui/view.h>
-
-typedef struct WaitConnection WaitConnection;
-
-WaitConnection* wait_connection_alloc();
-void wait_connection_free(WaitConnection* wait_connection);
-View* wait_connection_get_view(WaitConnection* wait_connection);
