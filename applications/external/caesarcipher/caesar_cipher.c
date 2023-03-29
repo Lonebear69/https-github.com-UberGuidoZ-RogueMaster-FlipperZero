b92679a9c33adea6c1e5aff1def1c0f711a8cb17
RogueMaster <anifan115@gmail.com>
2023-03-28 22:31:07 -0400

    mutex changes by panki27

--- applications/external/caesarcipher/caesar_cipher.c
+++ applications/external/caesarcipher/caesar_cipher.c
@@ -21,7 +21,6 @@ typedef struct {
 } PluginEvent;
 
 typedef struct {
-    FuriMutex* mutex;
     ViewDispatcher* view_dispatcher;
     TextInput* text_input;
     TextBox* text_box;
@@ -59,10 +58,7 @@ static void build_output(char* input, char* output) {
 }
 
 static void text_input_callback(void* ctx) {
-    furi_assert(ctx);
-    CaesarState* caesar_state = ctx;
-    furi_mutex_acquire(caesar_state->mutex, FuriWaitForever);
-
+    CaesarState* caesar_state = acquire_mutex((ValueMutex*)ctx, 25);
     FURI_LOG_D("caesar_cipher", "Input text: %s", caesar_state->input);
     // this is where we build the output.
     string_to_uppercase(caesar_state->input);
@@ -71,14 +67,13 @@ static void text_input_callback(void* ctx) {
     text_box_set_text(caesar_state->text_box, caesar_state->output);
     view_dispatcher_switch_to_view(caesar_state->view_dispatcher, 1);
 
-    furi_mutex_release(caesar_state->mutex);
+    release_mutex((ValueMutex*)ctx, caesar_state);
 }
 
 static bool back_event_callback(void* ctx) {
-    const CaesarState* caesar_state = ctx;
-    furi_mutex_acquire(caesar_state->mutex, FuriWaitForever);
+    const CaesarState* caesar_state = acquire_mutex((ValueMutex*)ctx, 25);
     view_dispatcher_stop(caesar_state->view_dispatcher);
-    furi_mutex_release(caesar_state->mutex);
+    release_mutex((ValueMutex*)ctx, caesar_state);
     return true;
 }
 
@@ -104,8 +99,8 @@ int32_t caesar_cipher_app() {
     FURI_LOG_D("caesar_cipher", "Running caesar_cipher_state_init");
     caesar_cipher_state_init(caesar_state);
 
-    caesar_state->mutex = furi_mutex_alloc(FuriMutexTypeNormal);
-    if(!caesar_state->mutex) {
+    ValueMutex state_mutex;
+    if(!init_mutex(&state_mutex, caesar_state, sizeof(CaesarState))) {
         FURI_LOG_E("caesar_cipher", "cannot create mutex\r\n");
         free(caesar_state);
         return 255;
@@ -115,7 +110,7 @@ int32_t caesar_cipher_app() {
     text_input_set_result_callback(
         caesar_state->text_input,
         text_input_callback,
-        caesar_state,
+        &state_mutex,
         caesar_state->input,
         TEXT_BUFFER_SIZE,
         //clear default text
@@ -140,12 +135,12 @@ int32_t caesar_cipher_app() {
     FURI_LOG_D("ceasar_cipher", "starting view dispatcher");
     view_dispatcher_set_navigation_event_callback(
         caesar_state->view_dispatcher, back_event_callback);
-    view_dispatcher_set_event_callback_context(caesar_state->view_dispatcher, caesar_state);
+    view_dispatcher_set_event_callback_context(caesar_state->view_dispatcher, &state_mutex);
     view_dispatcher_switch_to_view(caesar_state->view_dispatcher, 0);
     view_dispatcher_run(caesar_state->view_dispatcher);
 
     furi_record_close("gui");
-    furi_mutex_free(caesar_state->mutex);
+    delete_mutex(&state_mutex);
     caesar_cipher_state_free(caesar_state);
 
     return 0;
